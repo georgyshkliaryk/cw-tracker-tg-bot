4f7307490e25c78fe458eb20cd14bb87db6fc543
Heorhi Shkliaryk <h.shkliaryk@sam-solutions.com>
2024-04-07 01:49:02 +0300

    add second endpoint

--- api/constants.js
+++ api/constants.js
@@ -4,12 +4,22 @@ export const TG_AUTH_TOKEN = process.env.TG_AUTH_TOKEN;
 export const COC_AUTH_TOKEN = process.env.COC_AUTH_TOKEN;
 export const PORT = process.env.PORT || 3000;
 export const API_SERVICE_URL = 'https://cocproxy.royaleapi.dev/v1';
-export const CLAN_ID = '%232GUYURGLL';
+export const CLAN_ID = '%23YLQQUY2U';
+export const CLAN_ID2 = '%232RGQL98GL';
 
 export const maxStarsPossible = 3;
 export const commands = {
-  cw: /\/cw/,
+  cw: '/cw',
+  cw2: '/cw2',
+  getBothCw() {
+    return new RegExp(`${this.cw}|${this.cw2}`);
+  },
 };
 export const endpoints = {
   currentWar: `${API_SERVICE_URL}/clans/${CLAN_ID}/currentwar`,
+  currentWar2: `${API_SERVICE_URL}/clans/${CLAN_ID2}/currentwar`,
+};
+export const commandsToEndpointMap = {
+  [commands.cw]: endpoints.currentWar,
+  [commands.cw2]: endpoints.currentWar2,
 };
--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 import express from 'express';
-import { PORT, TG_AUTH_TOKEN, API_SERVICE_URL, CLAN_ID, maxStarsPossible, commands, endpoints } from './constants.js';
+import { PORT, TG_AUTH_TOKEN, maxStarsPossible, commands, commandsToEndpointMap, endpoints } from './constants.js';
 import { getTimeLeftText, getWarResultsText } from './helpers.js';
 import { sendRequest } from './request.js';
 
@@ -16,17 +16,15 @@ app.listen(PORT, () => {
 
 const bot = new TelegramBot(TG_AUTH_TOKEN, { polling: true });
 
-bot.onText(commands.cw, async (msg) => {
-  const chatId = msg.chat.id;
-  try {
-    const data = await sendRequest(endpoints.currentWar);
-    const { state, teamSize, attacksPerMember, startTime, endTime, clan, opponent } = data;
+const getCwInfo = async (endpoint) => {
+  const data = await sendRequest(endpoint);
+  const { state, teamSize, attacksPerMember, startTime, endTime, clan, opponent } = data;
 
-    let message = '';
-    if (state === 'notInWar') {
-      message = `${clan.name} is not in war`;
-    }
-    message = `
+  let message = '';
+  if (state === 'notInWar') {
+    message = `${clan.name} is not in war`;
+  }
+  message = `
 ${clan.name} vs ${opponent.name}
 ${getWarResultsText(state, clan.stars, opponent.stars, clan.destructionPercentage, opponent.destructionPercentage)}
 ${clan.stars} : ${opponent.stars}
@@ -42,6 +40,19 @@ stars: ${opponent.stars}/${maxStarsPossible * teamSize}
 attacks: ${opponent.attacks}/${attacksPerMember * teamSize}
 destruction: ${opponent.destructionPercentage}%
 `;
+
+  return message;
+};
+
+bot.onText(commands.getBothCw(), async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  try {
+    const endpoint = commandsToEndpointMap[text];
+    if (!endpoint) {
+      return;
+    }
+    const message = await getCwInfo(endpoint);
     bot.sendMessage(chatId, message);
   } catch (error) {
     bot.sendMessage(chatId, `Failed to fetch data, try again later. [${error.message}]`);
